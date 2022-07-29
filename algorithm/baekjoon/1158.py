9f896d709b653f44d0cd92f67bbb50f780cdda39
hcs4125 <hcs4125@gmail.com>
2022-07-29 16:52:08 +0900

    1158:solved

--- algorithm/baekjoon/1158.py
+++ algorithm/baekjoon/1158.py
@@ -6,7 +6,7 @@ answer_list = []
 
 for i in range(N):
     k_index = K - 1
-    if len(num_list) >= K:
+    if len(num_list) > K:
         answer_list.append(num_list[k_index])
         del num_list[k_index]
         front_list = num_list[:k_index]
@@ -15,9 +15,16 @@ for i in range(N):
     elif len(num_list) == 1:
         answer_list.append(num_list[0])
     else:
-        target = num_list[K - len(num_list) - 1]
+        k_index = (k_index % len(num_list))
+        target = num_list[k_index]
         answer_list.append(target)
         num_list.remove(target)
-    print(answer_list)
+        front_list = num_list[:k_index]
+        lear_list = num_list[k_index:]
+        num_list = lear_list + front_list
 
-print(num_list)
+print('<',end='')
+for i in range(len(answer_list) - 1):
+    print(answer_list[i],end=', ')
+print(answer_list[len(answer_list) - 1],end='')
+print('>')
